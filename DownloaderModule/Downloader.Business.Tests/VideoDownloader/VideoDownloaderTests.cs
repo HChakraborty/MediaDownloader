45e71a38748721e13aa49dea275bda26d7669509
HChakraborty <39049037+HChakraborty@users.noreply.github.com>
2025-05-18 20:49:47 +0530

    Added test projects (#18)
    * Added test projects
    
    * fixed formatting

---
+++ DownloaderModule/Downloader.Business.Tests/VideoDownloader/VideoDownloaderTests.cs
@@ -0,0 +1,9 @@
+﻿namespace Downloader.Business.Tests.VideoDownloader
+{
+    [TestClass]
+    public sealed class VideoDownloaderTests
+    {
+        [TestMethod]
+        public void VideoDownloaderReturnsFile() { }
+    }
+}
---
+++ DownloaderModule/Downloader.Service.Tests/VideoDownloadControllerTests.cs
@@ -0,0 +1,9 @@
+﻿namespace Downloader.Service.Tests
+{
+    [TestClass]
+    public sealed class VideoDownloadControllerTests
+    {
+        [TestMethod]
+        public void VideoDownloaderControllerReturnsOk() { }
+    }
+}
