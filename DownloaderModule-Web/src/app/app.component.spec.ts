45c5a51f86086c4b093fac9091fa273ff6d5b544
HChakraborty <hchakraborty2708@gmail.com>
2025-05-18 21:45:12 +0530

    updated test case

--- DownloaderModule-Web/src/app/app.component.spec.ts
+++ DownloaderModule-Web/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { RouterOutlet } from '@angular/router';
+import { HomePageModule } from './components/home-page/home-page.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, HomePageComponent],
+      declarations: [AppComponent],
+      imports: [RouterOutlet, HomePageModule]
     }).compileComponents();
   });
 
